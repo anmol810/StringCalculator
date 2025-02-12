a38c9cf7d58ca4f71dc726a68773deb452a7f868
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-12 16:07:19 +0530

    Code: add function returns sumation of numbers in a string having , and \n

--- src/string-calculator/string-calculator.js
+++ src/string-calculator/string-calculator.js
@@ -1,12 +1,13 @@
 const {
   computeSum,
+  isNegativeValuePresentInArray,
+  parseInput,
 } = require("../../helper/string-calculator-function/string-calculator-helper");
-const { delimiter } = require("../../constant/string-constant");
 function add(userInput) {
   // Returns 0 if the input is an empty string
   if (!userInput || !userInput.length) return 0;
-  // Split numbers and convert to integers
-  const numArray = userInput.split(delimiter).map(Number);
-  return computeSum(numArray);
+  //parsing the userInput
+  const inputArray = parseInput(userInput);
+  return computeSum(inputArray);
 }
 module.exports = add;
