3f2a0ef25e522f9675cfde4a3e3dd63546dd74a0
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-12 14:38:09 +0530

    Test: returns sumation of string with multiple value

--- test/string-calculator/string-calculator.test.js
+++ test/string-calculator/string-calculator.test.js
@@ -2,3 +2,6 @@ const add = require("../../src/string-calculator/string-calculator");
 test("returns 0 when the user input is empty string", () => {
   expect(add("")).toBe(0);
 });
+test("returns sumation of string with multiple input value", () => {
+  expect(add("1,2,3")).toBe(6);
+});
