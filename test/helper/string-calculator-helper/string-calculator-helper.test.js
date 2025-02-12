3ef557a88d0437f7c8577d6b30c48012e922ecc8
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-12 14:07:31 +0530

    Test: isNegativeValuePresentInArray funtion to check for negative value

--- test/helper/string-calculator-helper/string-calculator-helper.test.js
+++ test/helper/string-calculator-helper/string-calculator-helper.test.js
@@ -1,9 +1,18 @@
 const {
   computeSum,
+  isNegativeValuePresentInArray,
 } = require("../../../helper/string-calculator-function/string-calculator-helper"); // Adjust path as needed
 
+// test to find the sum of the array
 describe("computeSum", () => {
   test("returns the sum of multiple positive numbers", () => {
     expect(computeSum([1, 2, 3, 4, 5])).toBe(15);
+  });
+});
+
+// test to find the negative value in the array
+describe("negative value check", () => {
+  test("returns false since array does not contains any negative value", () => {
+    expect(isNegativeValuePresentInArray([1, 2, 3, 4, 5])).toBe(false);
   });
 });
