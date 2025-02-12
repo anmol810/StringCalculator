c7468af7b4a55fd35b7acfadfec56de5085b2bfd
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-12 15:55:37 +0530

    Code: parseInput function return an array after parsing the userInput containig , and \n

--- helper/string-calculator-function/string-calculator-helper.js
+++ helper/string-calculator-function/string-calculator-helper.js
@@ -1,3 +1,5 @@
+const { delimiter } = require("../../constant/string-constant");
+
 function computeSum(nums) {
   return nums.reduce((sum, curr) => sum + curr, 0);
 }
@@ -6,4 +8,14 @@ function isNegativeValuePresentInArray(nums) {
   if (!negativeNumberArray.length) return false;
   else return true;
 }
-module.exports = { computeSum, isNegativeValuePresentInArray };
+function parseInput(userInput) {
+  return userInput
+    .split(delimiter)
+    .filter((value) => {
+      return value.length > 0;
+    })
+    .map((value) => {
+      return Number(value);
+    });
+}
+module.exports = { computeSum, isNegativeValuePresentInArray, parseInput };
