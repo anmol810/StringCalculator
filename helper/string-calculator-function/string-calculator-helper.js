66a727708ef1a67df88b47c4b65ba8a85a211295
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-12 14:14:53 +0530

    Code: to check if the negative is present in the array return false if a negative number is not present

--- helper/string-calculator-function/string-calculator-helper.js
+++ helper/string-calculator-function/string-calculator-helper.js
@@ -1,4 +1,8 @@
 function computeSum(nums) {
   return nums.reduce((sum, curr) => sum + curr, 0);
 }
-module.exports = { computeSum };
+function isNegativeValuePresentInArray(nums) {
+  const negativeNumberArray = nums.filter((num) => num < 0);
+  if (!negativeNumberArray.length) return false;
+}
+module.exports = { computeSum, isNegativeValuePresentInArray };
